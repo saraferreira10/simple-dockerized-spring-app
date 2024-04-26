8ed6d82e0931316741b615dadf2f2004130db848
Sara Ferreira <sara.f.araujo12@gmail.com>
2024-04-26 13:40:28 -0300

    project created

---
+++ src/main/java/com/sara/dockerspringboot/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package com.sara.dockerspringboot.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String SayHello() {
+        return "Hello spring + docker";
+    }
+}
